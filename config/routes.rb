5e7949d91768a8cdc197dd8e5b82f34029531e4f
souson7 <souson7@users.noreply.github.com>
2025-09-22 06:41:02 +0900

    マイページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users, only:[:index, :show]
   resources :posts do
     resources :comments, only:[:create, :destroy]
+    resource :favorites, only:[:create, :destroy]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_18_132640) do
+ActiveRecord::Schema[7.1].define(version: 2025_09_21_141807) do
   create_table "comments", force: :cascade do |t|
     t.text "comment"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "post_id"
     t.datetime "created_at", null: false
@@ -26,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_18_132640) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.string "image_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
